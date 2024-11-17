d6872b8779bc25669a31e8d8c9a8475495cde019
jeje01 <jje141@gmail.com>
2024-11-17 23:17:46 +0900

    Initial Commit

---
+++ next.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false,
+  transpilePackages: [
+		"antd",
+		"@ant-design",
+		"rc-util",
+		"rc-pagination",
+		"rc-picker",
+		"rc-notification",
+		"rc-tooltip",
+	],
+};
+
+export default nextConfig;
