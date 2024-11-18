37921e73f709adfd43e67b6e510cd7bee5ccdaae
jeje01 <jje141@gmail.com>
2024-11-18 22:07:20 +0900

    fix: NumberTag 스타일 수정

--- src/components/NumberTag/index.tsx
+++ src/components/NumberTag/index.tsx
@@ -1,7 +1,11 @@
-export const NumberTag = ({num} : { num: number}) => {
-  const backgroundColor = num === 1 ? "#ff570f" : "white"
+export const NumberTag = ({ num }: { num: number }) => {
+  const backgroundColor = num === 1 ? "[#ff570f]" : "white";
 
   return (
-    <div className={`rounded-[10px] bg-[${backgroundColor}] text-black text-xs font-bold w-[18px] h-[18px] flex items-center justify-center`}>{num}</div>
-  )
-}
+    <div
+      className={`rounded-[10px] bg-${backgroundColor} text-black text-xs font-bold w-[18px] h-[18px] flex items-center justify-center`}
+    >
+      {num}
+    </div>
+  );
+};
